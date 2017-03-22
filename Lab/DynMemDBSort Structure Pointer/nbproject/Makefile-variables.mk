44e48c216ce4c959ee025c5f64b131de0081a909
garciaa3196 <garciaa0461@gmail.com>
2017-03-22 11:57:32 -0700

    Signed-off-by: garciaa3196 <garciaa0461@gmail.com>

---
+++ Lab/DynMemDBSort Structure Pointer/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=dynmemdbsort_structure_pointer
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/dynmemdbsort_structure_pointer
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=dynmemdbsortstructurepointer.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/dynmemdbsortstructurepointer.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=dynmemdbsort_structure_pointer
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/dynmemdbsort_structure_pointer
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=dynmemdbsortstructurepointer.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/dynmemdbsortstructurepointer.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
